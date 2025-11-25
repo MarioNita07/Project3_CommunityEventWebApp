a27f9599881969461928eee7b84d3b02ef0ba6d6
NitaMario <marionita2664@gmail.com>
2025-11-25 04:15:18 +0200

    Add project files.

---
+++ Models/CommunityEventContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CommunityEvents.Models
+{
+    public class CommunityEventContext : DbContext
+    {
+        public CommunityEventContext(DbContextOptions<CommunityEventContext> options)
+            : base(options)
+        { }
+        
+        public DbSet<Event>? Events { get; set; }
+        public DbSet<Review>? Reviews { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Registration>? Registrations { get; set; }
+
+
+    }
+}
---
+++ Models/Registration.cs
@@ -0,0 +1,11 @@
+﻿namespace CommunityEvents.Models
+{
+    public class Registration
+    {
+        public int RegistrationId { get; set; }
+        public DateOnly RegistrationDate { get; set; }
+        public int EventId { get; set; }
+        public Event? Event { get; set; }
+
+    }
+}
